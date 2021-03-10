a4fd8abdaea5f7c597ba27a717daa23430228700
Leo <leo19940905@gmail.com>
2021-03-10 15:57:32 +0800

    888

--- src/env_test.js
+++ src/env_test.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config();    //環境變數
 
 console.log(process.env.DB_USER);
 console.log(process.env.DB_PASS);
--- src/index.js
+++ src/index.js
@@ -4,20 +4,56 @@ const fs = require("fs/promises")
 const app = express();
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: false }));         //用form url傳
+app.use(express.json());                                  //用json傳
+
+
+
 app.use(express.static("public"));   //將public資料夾設為根目錄,底下的檔案都能從網址直接叫
+//用use是把REST方法都包含在內(GET,POST...)
+
+
 
 app.get("/", (req, res) => {
     res.render("home", { name: "Leo" })
-    // res.send(`<h2>Hello</h2>`);
 })
+
+
+
+app.get("/json-sales", (req, res) => {                        //第一個斜線後代表網址         
+    const sales = require(__dirname + "/../data/json-sales"); //callback去data/json-sales抓資料
+    res.render("json-sales", { sales })                       //去json-sales.ejs,把抓回來的資料傳過去
+})
+
+
+
+
+app.get("/try-qs", (req, res) => {                            //query string
+    res.json(req.query);
+})
+
+
+
+
+// const urlencodedParser = express.urlencoded({ extended: false });     //Middleware
+// app.post("/try-post", urlencodedParser, (req, res) => {
+
+//     res.json(req.body);
+// })
+
+
+app.post("/try-post", (req, res) => {
+    res.json(req.body);
+})
+
+
+
 
 app.use(async (req, res) => {
     const error = await fs.readFile(__dirname + "/../public/404.html")
     res.status(404).send(error.toString());
-
 })
 
 app.listen(process.env.PORT || 3000, () => {
-
     console.log("Server starts:", new Date());
 })
