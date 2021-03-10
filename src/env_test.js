c46588663d708492c877d644cfacce3a2b5da8d4
Leo <leo19940905@gmail.com>
2021-03-10 10:00:19 +0800

    777

---
+++ src/env_test.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+console.log(process.env.HEHE);
--- src/http_server03.js
+++ src/http_server03.js
@@ -5,5 +5,5 @@ const server = http.createServer(async (req, res) => {
     const str = await fs.readFile(__dirname + "/Headers.json");
     res.end(str);
 })
-
+const a = require("")
 server.listen(3000);
